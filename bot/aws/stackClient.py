38debb2489bc7de3a7b2d7c048010df882a57913
Emily F <foxxemusicmail@gmail.com>
2020-12-29 02:23:55 +0000

    fix: IP mapping

--- bot/aws/stackClient.py
+++ bot/aws/stackClient.py
@@ -71,7 +71,7 @@ class StackClient():
   async def get_ec2_instance(self, name):
     async with self._session.create_client('ec2') as client:
       instances = await client.describe_instances()
-      reservation = single(lambda reservation: reservation_stack_name(reservation) == name, instances['Reservations'])
+      reservation = single(lambda reservation: reservation_stack_name(reservation) == name and reservation['Instances'][0]['State']['Name'] == 'running', instances['Reservations'])
       return reservation['Instances'][0]
 
 def reservation_stack_name(reservation):
