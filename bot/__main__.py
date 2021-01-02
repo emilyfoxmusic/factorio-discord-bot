ed3a961e95ca93b819da6c7081267fe36b456373
Emily F <foxxemusicmail@gmail.com>
2021-01-02 21:49:13 +0000

    chore: logging config

--- bot/__main__.py
+++ bot/__main__.py
@@ -1,5 +1,8 @@
+import logging
 import os
 from bot.bot import bot
+
+logging.basicConfig(format='%(asctime)s:%(module)s:%(levelname)s:%(message)s', filename="bot.log", level=logging.INFO)
 
 DISCORD_TOKEN = os.getenv('DISCORD_TOKEN')
 bot.run(DISCORD_TOKEN)
