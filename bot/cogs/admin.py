a1e5dadaddf0101024b0c15a17741bd83e793b56
Emily F <foxxemusicmail@gmail.com>
2021-01-10 23:57:49 +0000

    feat: add category and manage channels automatically

--- bot/cogs/admin.py
+++ bot/cogs/admin.py
@@ -1,8 +1,9 @@
 import re
+import discord
 from discord.ext import commands
 from ..services import channelMappingService, gameService
 from ..utilities import random_string
-from .roles import ADMIN_ROLE
+from .roles import FACTORIO_CATEGORY
 
 
 name_pattern = re.compile("^[A-Za-z][A-Za-z0-9-]*$")
@@ -17,17 +18,19 @@ class Admin(commands.Cog):
     await channelMappingService.init_channel_table()
 
   @commands.command(help='Create a new game')
-  @commands.has_role(ADMIN_ROLE)
   async def new(self, ctx, name, version, *mods):
     if not name_pattern.match(name):
       await ctx.send(f'Name must only contain letters, numbers and -, and must start with a letter. :no_entry:')
       return
     await ctx.send(f'Creating new game: {name} :star2:')
     await gameService.create_game(name, version, *mods)
-    await ctx.send(f'Created {name}! Assign a channel with `!set-channel` and use `!start` to get the party started :partying_face:')
+    for guild in self.bot.guilds:
+      category = discord.utils.get(guild.categories, name=FACTORIO_CATEGORY)
+      channel = await category.create_text_channel(name)
+      await channelMappingService.set_channel_mapping(name, guild.id, channel.id)
+    await ctx.send(f"Created {name}! Let's get this party started! :partying_face:")
 
   @commands.command(help='Delete a game')
-  @commands.has_role(ADMIN_ROLE)
   async def delete(self, ctx, name, confirmation_phrase=None):
     if confirmation_phrase != None and confirmation_phrase == self.confirmation_phrases.get(name):
       del self.confirmation_phrases[name]
@@ -37,6 +40,11 @@ class Admin(commands.Cog):
       # We want to remove any channel associations with this game - the easiest way to do that
       # is just to validate the mappings again
       await channelMappingService.validate_mappings(self.bot)
+      for guild in self.bot.guilds:
+        category = discord.utils.get(guild.categories, name=FACTORIO_CATEGORY)
+        channel = discord.utils.get(category.channels, name=name)
+        if channel != None:
+          await channel.delete()
       await ctx.send(f'Deleted {name}! :skull_crossbones:')
     elif confirmation_phrase == None or self.confirmation_phrases.get(name) == None:
       self.confirmation_phrases[name] = random_string(10)
@@ -46,7 +54,6 @@ class Admin(commands.Cog):
       await ctx.send(f':no_entry_sign: Confirmation phrase did not match - to confirm the delete, use `!delete {name} {self.confirmation_phrases[name]}`')
 
   @commands.command(name='set-game', help='Link the current channel to the specified game')
-  @commands.has_role(ADMIN_ROLE)
   async def set_game(self, ctx, name):
     if await gameService.game_exists(name):
       await channelMappingService.set_channel_mapping(name, ctx.channel.guild.id, ctx.channel.id)
--- bot/cogs/roles.py
+++ bot/cogs/roles.py
@@ -2,7 +2,8 @@ import discord
 from discord.ext import commands
 
 
-ADMIN_ROLE = 'Factorio-Admin'
+FACTORIO_ROLE = 'Factorio'
+FACTORIO_CATEGORY = 'Factorio servers'
 
 class Roles(commands.Cog):
   def __init__(self, bot):
@@ -11,7 +12,47 @@ class Roles(commands.Cog):
   @commands.Cog.listener()
   async def on_ready(self):
     for guild in self.bot.guilds:
-      roles = await guild.fetch_roles()
-      admin_role = discord.utils.get(roles, name=ADMIN_ROLE)
-      if admin_role == None:
-        await guild.create_role(name=ADMIN_ROLE, mentionable=True)
+      await create_role(guild)
+      await create_category(guild, self.bot)
+
+  @commands.command(help='Assign yourself the factorio role and get involved!', name="count-me-in")
+  async def count_me_in(self, ctx):
+    role = await get_factorio_role(ctx.guild)
+    await ctx.author.add_roles(role)
+    await ctx.send(f'Yay, welcome {ctx.author.mention}. I\'ve messaged you some more instructions :smiley:')
+    await ctx.author.send("Hello! You should now be able to see any existing games under the 'factorio servers' category in discord (if any exist).\nI'm here to help you manage your games - I can create servers (with whatever mods you'd like), start/stop your games, take backups, and I'll even automatically stop servers when no one's using them! Use `!help` for details on the commands I understand. (I can do this in DM for your convenience.) Enjoy factorio-ing and let me know if there's anything I can help with! :partying_face:")
+
+  @commands.command(help='Removes the factorio role so you won\'t see the games', name="count-me-out")
+  async def count_me_out(self, ctx):
+    role = await get_factorio_role(ctx.guild)
+    await ctx.author.remove_roles(role)
+    await ctx.send(f'Okay, come back soon! :cry:')
+
+async def create_role(guild):
+  role = await get_factorio_role(guild)
+  if role == None:
+    role = await guild.create_role(name=FACTORIO_ROLE, mentionable=False)
+
+async def create_category(guild, bot):
+  category = discord.utils.get(guild.categories, name=FACTORIO_CATEGORY)
+  if category == None:
+    factorio_role = await get_factorio_role(guild)
+    bot_role = get_bot_role(guild, bot)
+    overwrites = {
+      guild.default_role: discord.PermissionOverwrite(view_channel=False),
+      factorio_role: discord.PermissionOverwrite(view_channel=True),
+      bot_role: discord.PermissionOverwrite(view_channel=True)
+    }
+    await guild.create_category(FACTORIO_CATEGORY, overwrites=overwrites)
+
+async def get_factorio_role(guild):
+  roles = await guild.fetch_roles()
+  return discord.utils.get(roles, name=FACTORIO_ROLE)
+
+def get_bot_role(guild, bot):
+  bot_role = list(filter(lambda role: not role.is_default(), guild.get_member(bot.user.id).roles))
+  if len(bot_role) > 1:
+    raise Exception('Bot has multiple roles')
+  if len(bot_role) == 0:
+    raise Exception('Bot role not found')
+  return bot_role[0]
