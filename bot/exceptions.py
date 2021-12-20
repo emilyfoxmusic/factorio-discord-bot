emilyfoxmusic/factorio-discord-bot

commit 8e5676be28a9e11319bbf1884964f70cebbdff77

style: #6 - install pylint and follow conventions

bot/exceptions.py
@@ -1,3 +1,2 @@
 class InvalidOperationException(Exception):
-  def __init__(self, message):
-    self.message = message
+    pass

.pylintrc
@@ -1,0 +1,4 @@
+[MESSAGES CONTROL]
+
+disable=C0114,C0115,C0116
+good-names=ip,fn

Pipfile
@@ -8,7 +8,7 @@ python_version = '3.9'
 
 [packages]
 aiobotocore = "~=1.1.2"
-discord.py = "~=1.5.1"
+discord = {py = "~=1.5.1"}
 python-dotenv = "~=0.15.0"
 api-client = "~=1.2.1"
 factorio-rcon-py = "~=1.2.1"
@@ -17,3 +17,4 @@ paramiko = "~=2.7.2"
 
 [dev-packages]
 pylint="*"
+autopep8 = "*"

bot/bot.py
@@ -1,13 +1,14 @@
 from discord.ext import commands
-from .cogs import admin, autoShutdown, backups, channelManagement, commandErrorHandler, game, healthcheck, roles
+from .cogs import (admin, auto_shutdown, backups, channel_management,
+                   command_error_handler, game, healthcheck, roles)
 
 
 bot = commands.Bot(command_prefix='!')
 
 bot.add_cog(admin.Admin(bot))
-bot.add_cog(autoShutdown.AutoShutdown(bot))
-bot.add_cog(channelManagement.ChannelManagement(bot))
-bot.add_cog(commandErrorHandler.CommandErrorHandler(bot))
+bot.add_cog(auto_shutdown.AutoShutdown(bot))
+bot.add_cog(channel_management.ChannelManagement(bot))
+bot.add_cog(command_error_handler.CommandErrorHandler(bot))
 bot.add_cog(game.Game(bot))
 bot.add_cog(healthcheck.Healthcheck(bot))
 bot.add_cog(roles.Roles(bot))

bot/clients/channelMappingClient.py
@@ -1,107 +1,0 @@
-import aiobotocore
-
-
-CHANNEL_TABLE = 'FactorioChannelMapping'
-
-_session = aiobotocore.get_session()
-
-async def check_table_exists():
-  async with _session.create_client('dynamodb') as client:
-    tables = await client.list_tables()
-    return CHANNEL_TABLE in tables['TableNames']
-
-async def create_table():
-  async with _session.create_client('dynamodb') as client:
-    await client.create_table(
-      AttributeDefinitions=[
-        {
-          'AttributeName': 'GuildId',
-          'AttributeType': 'S'
-        },
-        {
-          'AttributeName': 'ChannelId',
-          'AttributeType': 'S'
-        },
-      ],
-      TableName=CHANNEL_TABLE,
-      KeySchema=[
-        {
-          'AttributeName': 'GuildId',
-          'KeyType': 'HASH'
-        },
-        {
-          'AttributeName': 'ChannelId',
-          'KeyType': 'RANGE'
-        },
-      ],
-      BillingMode='PAY_PER_REQUEST')
-    await client.get_waiter('table_exists').wait(
-      TableName=CHANNEL_TABLE,
-      WaiterConfig={
-        'Delay': 10
-      })
-
-async def get_associated_game(guild_id, channel_id):
-  async with _session.create_client('dynamodb') as client:
-    mapping_for_channel = await client.get_item(
-      TableName=CHANNEL_TABLE,
-      Key={
-        'GuildId': { 'S' : str(guild_id) },
-        'ChannelId': { 'S': str(channel_id) }
-      }
-    )
-    if 'Item' in mapping_for_channel:
-      return mapping_for_channel['Item']['GameName']['S']
-    return None
-
-async def get_mappings(name):
-  async with _session.create_client('dynamodb') as client:
-    mappings_response = await client.scan(
-      TableName=CHANNEL_TABLE,
-      FilterExpression="GameName = :gamename",
-      ExpressionAttributeValues={ ":gamename": { "S": name } }
-    )
-  return map(map_item, mappings_response['Items'])
-
-async def set_mapping(name, guild_id, channel_id):
-    async with _session.create_client('dynamodb') as client:
-      await client.put_item(
-        TableName=CHANNEL_TABLE,
-        Item={
-          'GuildId': { 'S' : str(guild_id) },
-          'ChannelId': { 'S': str(channel_id) },
-          'GameName': { 'S': name }
-        })
-
-async def delete_guild_mappings(guild_id):
-  async with _session.create_client('dynamodb') as client:
-    mappings_response = await client.scan(
-      TableName=CHANNEL_TABLE,
-      KeyConditionExpression="GuildId = :guildid",
-      ExpressionAttributeValues={ ":guildid": { "S": str(guild_id) } }
-    )
-  if 'Items' in mappings_response and len(mappings_response['Items']) > 0:
-    await delete_mappings(map(map_item, mappings_response['Items']))
-
-async def delete_mappings(mappings):
-  async with _session.create_client('dynamodb') as client:
-      await client.batch_write_item(
-        RequestItems={
-          CHANNEL_TABLE: list(map(lambda mapping: {
-            'DeleteRequest': {
-              'Key': {
-                'GuildId': { 'S' : str(mapping['guild_id']) },
-                'ChannelId': { 'S': str(mapping['channel_id']) }
-              }
-            }
-          }, mappings))
-        }
-      )
-
-async def get_all_mappings():
-  async with _session.create_client('dynamodb') as client:
-    mappings_response = await client.scan(TableName=CHANNEL_TABLE)
-    return map(map_item, mappings_response.get('Items'))
-
-def map_item(item):
-  return { 'guild_id': int(item['GuildId']['S']), 'channel_id': int(item['ChannelId']['S']), 'game': item['GameName']['S'] }

bot/clients/channel_mapping_client.py
@@ -1,0 +1,120 @@
+import aiobotocore
+
+
+CHANNEL_TABLE = 'FactorioChannelMapping'
+
+_session = aiobotocore.get_session()
+
+
+async def check_table_exists():
+    async with _session.create_client('dynamodb') as client:
+        tables = await client.list_tables()
+        return CHANNEL_TABLE in tables['TableNames']
+
+
+async def create_table():
+    async with _session.create_client('dynamodb') as client:
+        await client.create_table(
+            AttributeDefinitions=[
+                {
+                    'AttributeName': 'GuildId',
+                    'AttributeType': 'S'
+                },
+                {
+                    'AttributeName': 'ChannelId',
+                    'AttributeType': 'S'
+                },
+            ],
+            TableName=CHANNEL_TABLE,
+            KeySchema=[
+                {
+                    'AttributeName': 'GuildId',
+                    'KeyType': 'HASH'
+                },
+                {
+                    'AttributeName': 'ChannelId',
+                    'KeyType': 'RANGE'
+                },
+            ],
+            BillingMode='PAY_PER_REQUEST')
+        await client.get_waiter('table_exists').wait(
+            TableName=CHANNEL_TABLE,
+            WaiterConfig={
+                'Delay': 10
+            })
+
+
+async def get_associated_game(guild_id, channel_id):
+    async with _session.create_client('dynamodb') as client:
+        mapping_for_channel = await client.get_item(
+            TableName=CHANNEL_TABLE,
+            Key={
+                'GuildId': {'S': str(guild_id)},
+                'ChannelId': {'S': str(channel_id)}
+            }
+        )
+        if 'Item' in mapping_for_channel:
+            return mapping_for_channel['Item']['GameName']['S']
+        return None
+
+
+async def get_mappings(name):
+    async with _session.create_client('dynamodb') as client:
+        mappings_response = await client.scan(
+            TableName=CHANNEL_TABLE,
+            FilterExpression="GameName = :gamename",
+            ExpressionAttributeValues={":gamename": {"S": name}}
+        )
+    return map(map_item, mappings_response['Items'])
+
+
+async def set_mapping(name, guild_id, channel_id):
+    async with _session.create_client('dynamodb') as client:
+        await client.put_item(
+            TableName=CHANNEL_TABLE,
+            Item={
+                'GuildId': {'S': str(guild_id)},
+                'ChannelId': {'S': str(channel_id)},
+                'GameName': {'S': name}
+            })
+
+
+async def delete_guild_mappings(guild_id):
+    async with _session.create_client('dynamodb') as client:
+        mappings_response = await client.scan(
+            TableName=CHANNEL_TABLE,
+            KeyConditionExpression="GuildId = :guildid",
+            ExpressionAttributeValues={":guildid": {"S": str(guild_id)}}
+        )
+    if 'Items' in mappings_response and len(mappings_response['Items']) > 0:
+        await delete_mappings(map(map_item, mappings_response['Items']))
+
+
+async def delete_mappings(mappings):
+    async with _session.create_client('dynamodb') as client:
+        await client.batch_write_item(
+            RequestItems={
+                CHANNEL_TABLE: list(map(lambda mapping: {
+                    'DeleteRequest': {
+                        'Key': {
+                            'GuildId': {'S': str(mapping['guild_id'])},
+                            'ChannelId': {'S': str(mapping['channel_id'])}
+                        }
+                    }
+                }, mappings))
+            }
+        )
+
+
+async def get_all_mappings():
+    async with _session.create_client('dynamodb') as client:
+        mappings_response = await client.scan(TableName=CHANNEL_TABLE)
+        return map(map_item, mappings_response.get('Items'))
+
+
+def map_item(item):
+    return {
+        'guild_id': int(item['GuildId']['S']),
+        'channel_id': int(item['ChannelId']['S']),
+        'game': item['GameName']['S']
+    }

bot/clients/ec2Client.py
@@ -1,17 +1,0 @@
-import aiobotocore
-import logging
-from ..utilities import single
-
-
-_session = aiobotocore.get_session()
-
-async def get_ec2_instance(name):
-  logging.info('Fetching info about EC2 instance for %s' % name)
-  async with _session.create_client('ec2') as client:
-    instances = await client.describe_instances()
-    reservation = single(lambda reservation: reservation_stack_name(reservation) == name and reservation['Instances'][0]['State']['Name'] == 'running', instances['Reservations'])
-    return reservation['Instances'][0]
-
-def reservation_stack_name(reservation):
-  tags = reservation['Instances'][0]['Tags']
-  return single(lambda tag: tag['Key'] == 'aws:cloudformation:stack-name', tags)['Value']

bot/clients/ec2_client.py
@@ -1,0 +1,23 @@
+import logging
+import aiobotocore
+from ..utilities import single
+
+
+_session = aiobotocore.get_session()
+
+
+async def get_ec2_instance(name):
+    logging.info('Fetching info about EC2 instance for %s', name)
+    async with _session.create_client('ec2') as client:
+        instances = await client.describe_instances()
+        reservation = single(lambda reservation:
+                             reservation_stack_name(reservation) == name and
+                             reservation['Instances'][0]['State']['Name'] == 'running',
+                             instances['Reservations']
+                             )
+        return reservation['Instances'][0]
+
+
+def reservation_stack_name(reservation):
+    tags = reservation['Instances'][0]['Tags']
+    return single(lambda tag: tag['Key'] == 'aws:cloudformation:stack-name', tags)['Value']

commit eacb335d89497307358daaab3034993bb1c934fa

fix: re-add InvalidOperationException message to fix error messages
(Broken in 8e5676be28a9e11319bbf1884964f70cebbdff77)

bot/exceptions.py
@@ -1,2 +1,4 @@
 class InvalidOperationException(Exception):
-    pass
+    def __init__(self, message):
+        super().__init__(message)
+        self.message = message